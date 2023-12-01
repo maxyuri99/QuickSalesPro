a70cfa01c6e33f555c3f079fafde1c05b4561b54
maxyuri99 <maxyurisantos@hotmail.com>
2023-12-01 17:27:31 -0300

    alteração do cors main

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './providers/UserContext.jsx'
-import axios from 'axios';
+import axios from 'axios'
 
-
+axios.interceptors.request.use((config) => {
+  config.headers['Access-Control-Allow-Origin'] = '*'
+  config.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE'
+  config.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+  return config
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
